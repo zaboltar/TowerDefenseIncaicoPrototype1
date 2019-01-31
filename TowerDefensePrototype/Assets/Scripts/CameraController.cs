b5e562fc457fca18234fa16f8fb6ebadeb9002ff
zaboltar <cletothar@gmail.com>
2019-01-31 12:35:11 -0500

    CamClamp

--- TowerDefensePrototype/Assets/Scripts/CameraController.cs
+++ TowerDefensePrototype/Assets/Scripts/CameraController.cs
@@ -2,66 +2,96 @@
 
 public class CameraController : MonoBehaviour {
 
-		// se comenta el "bloqueo" de camara con escape... es mejor clampear!
+    // se comenta el "bloqueo" de camara con escape... es mejor clampear!
 
-	//private bool doMovement = true;
+    //private bool doMovement = true;
 
-	public float panSpeed = 30f;
-	public float panBorderThickness = 10f;	
+    public float panSpeed = 30f;
+    public float panBorderThickness = 10f;
 
-	public float scrollSpeed = 5f;
-	public float minY = 10f;
-	public float maxY = 80f;
+    public float scrollSpeed = 5f;
+    public float minY = 10f;
+    public float maxY = 80f;
+
+    private Vector3 startPosition;
+
+    public Vector3 offSet;
+
+    void Start()
+    {
+        startPosition = transform.position;
+    }
 
 
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
-			if (GameManager.gameEnded) {
-				this.enabled = false;
-				return;
-			}
+        /*if (GameManager.gameEnded) {
+            this.enabled = false;
+            return;
+        }*/
 
-			/*if (Input.GetKeyDown(KeyCode.Escape))
-			doMovement = !doMovement;
+        /*if (Input.GetKeyDown(KeyCode.Escape))
+        doMovement = !doMovement;
 
-			if (!doMovement) {
-				return;
-			}*/
-			
-			if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness) {
+        if (!doMovement) {
+            return;
+        }*/
 
-				transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
-			}
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
 
-			if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness) {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
 
-				transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
-			}
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
 
-			if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness) {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
 
-				transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
-			}
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
 
-			if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness) {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
 
-				transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
-			}
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
 
-			float scroll = Input.GetAxis("Mouse ScrollWheel"); 
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
 
-			Vector3 pos = transform.position;
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
 
-			pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
-			pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        Vector3 pos = transform.position;
 
-			transform.position = pos;
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
 
-	}
+        transform.position = pos;
+
+    }
+
+    public void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+
+        if (pos.x < startPosition.x - offSet.x)
+            pos.x = startPosition.x - offSet.x;
+
+        if (pos.x > startPosition.x + offSet.x)
+            pos.x = startPosition.x + offSet.x;
+
+        if (pos.z < startPosition.z - offSet.z)
+            pos.z = startPosition.z - offSet.z;
+
+        if (pos.z > startPosition.z + offSet.z)
+            pos.z = startPosition.z + offSet.z;
+
+        transform.position = pos;
+    }
 }
-
-
 
 // falta el clamp del paneo
