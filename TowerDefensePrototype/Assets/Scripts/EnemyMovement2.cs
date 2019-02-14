656d5c7654475c9cf965211dbea859339af145a0
zaboltar <cletothar@gmail.com>
2019-02-14 12:43:05 -0500

    flippingX

--- TowerDefensePrototype/Assets/Scripts/EnemyMovement2.cs
+++ TowerDefensePrototype/Assets/Scripts/EnemyMovement2.cs
@@ -8,24 +8,44 @@ public class EnemyMovement2 : MonoBehaviour {
 	private Transform target;
 	private int wavepointIndex = 0;
 
+    private Vector3 lastEnemyPos;
+    private SpriteRenderer rend;
+
 	private EnemyMovement enemy;
 
-		void Start () {
+	void Start () {
 
-			enemy = GetComponent<EnemyMovement>();
-			target = Waypoints.points[0];
-		}	
+        lastEnemyPos = transform.position;
+		enemy = GetComponent<EnemyMovement>();
+		target = Waypoints.points[0];
+        rend = transform.GetChild(1).GetComponent<SpriteRenderer>();
 
-			void Update () {
-			Vector3 dir = target.position - transform.position;
-			transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+	}	
 
-				if (Vector3.Distance(transform.position, target.position) <= 0.4f ) {
-					GetNextWaypoint();
-				}
+	void Update () {
+        // se guarda el mov en el start
 
-				enemy.speed = enemy.startSpeed;
+        // mover personaje
+		Vector3 dir = target.position - transform.position;
+		transform.Translate(dir.normalized * enemy.speed * Time.deltaTime, Space.World);
+
+		if (Vector3.Distance(transform.position, target.position) <= 0.4f ) {
+			GetNextWaypoint();
 		}
+
+		enemy.speed = enemy.startSpeed;
+
+        // pregunta por FlipX
+
+        if ((transform.position.x - lastEnemyPos.x) > 0 && rend.flipX) {
+            rend.flipX = false;
+        } else if ((transform.position.x - lastEnemyPos.x) < 0 && !rend.flipX) {
+            rend.flipX = true;
+        }
+
+        // guardar mov de nuevo
+        lastEnemyPos = transform.position;
+	}
 
 		void GetNextWaypoint() {
 
@@ -39,8 +59,10 @@ public class EnemyMovement2 : MonoBehaviour {
 		}
 
 		void EndPath(){
+        
 			PlayerStats.Lives--;
 			WaveSpawner.EnemiesAlive--;
 			Destroy(gameObject);
+
 		}
 }
