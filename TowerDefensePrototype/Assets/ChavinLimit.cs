f5ae75b061dfe60d0d4be8a8005c7068eef7951c
zaboltar <cletothar@gmail.com>
2019-03-01 22:51:24 -0500

    plzDontCrash

---
+++ TowerDefensePrototype/Assets/ChavinLimit.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChavinLimit : MonoBehaviour {
+
+    public GameObject textDisplay;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textDisplay.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            textDisplay.SetActive(false);
+        }
+    }
+}
---
+++ TowerDefensePrototype/Assets/PlayerImpro.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerImpro : MonoBehaviour {
+
+    Rigidbody myRB;
+    private Vector3 lastEnemyPos;
+    private SpriteRenderer rend;
+
+    // Use this for initialization
+    void Start () {
+        myRB = GetComponent<Rigidbody>();
+        lastEnemyPos = transform.position;
+        rend = transform.GetChild(1).GetComponent<SpriteRenderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x--;
+            this.transform.position = position;
+        }
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Vector3 position = this.transform.position;
+            position.x++;
+            this.transform.position = position;
+        }
+
+        if ((transform.position.x - lastEnemyPos.x) > 0 && rend.flipX)
+        {
+            rend.flipX = false;
+        }
+        else if ((transform.position.x - lastEnemyPos.x) < 0 && !rend.flipX)
+        {
+            rend.flipX = true;
+        }
+
+        lastEnemyPos = transform.position;
+
+
+    }
+
+    /*void FixedUpdate ()
+    {
+        if (Input.GetButtonDown(KeyCode.D) || (Input.GetButtonDown(KeyCode.RightArrow)) {
+
+        }
+    }*/
+}
