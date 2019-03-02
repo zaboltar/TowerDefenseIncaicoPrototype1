1beee8905e0e9dcee212b454c81fb985fd1bbe0c
zaboltar <cletothar@gmail.com>
2019-03-02 03:39:49 -0500

    dialogueManagerLol

---
+++ TowerDefensePrototype/Assets/Scripts/DialogueZone.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueZone : MonoBehaviour {
+
+    public GameObject canvasTxt;
+    public Dialogue dialogue;
+    private SFXManager sfxMan;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sfxMan = GetComponent<SFXManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canvasTxt.SetActive(true);
+
+            /*if (Input.GetKeyDown(KeyCode.E))
+            {
+                FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+            }*/
+
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            canvasTxt.SetActive(false);
+
+        }
+    }
+
+    public void StartTalk()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+    }
+
+    public void Kill ()
+    {
+        gameObject.SetActive(false);
+       
+    }
+}
